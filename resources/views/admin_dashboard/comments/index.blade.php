347191475fa79b198985bc312f93bba759c7af60
alminana <45486184+alminana@users.noreply.github.com>
2023-05-14 17:19:01 +0300

    Update index.blade.php

--- resources/views/admin_dashboard/comments/index.blade.php
+++ resources/views/admin_dashboard/comments/index.blade.php
@@ -44,7 +44,7 @@
                                         <th>View Comment</th>
 									
 										<th>Created at</th>
-										<th>Actions</th>
+								
 									</tr>
 								</thead>
 								<tbody>
@@ -74,46 +74,7 @@
                                             <a target='_blank' class='btn btn-primary btn-sm' href="{{ route('posts.show', $comment->post->id) }}#comment_{{ $comment->id }}">View Comment</a>
                                         </td>
                                         <td>{{ $comment->created_at->diffForHumans() }}</td>
-                                        <td>
-											<div class="d-flex order-actions">
-												<a href="{{ route('admin.comments.edit', $comment) }}" class=""><i class='bx bxs-edit'></i></a>
-												<!-- <a href="#" onclick="event.preventDefault(); document.getElementById('delete_form_{{ $comment->id }}').submit();" class="ms-3"><i class='bx bxs-trash'></i></a>
-											
-                                                <form method='post' action="{{ route('admin.comments.destroy', $comment) }}" id='delete_form_{{ $comment->id }}'>@csrf @method('DELETE')</form> -->
-
-												{{-- <form method='post' action="{{ route('admin.comments.destroy', $comment) }}" id='delete_form_{{ $comment->id }}'>
-												@csrf 
-												@method('DELETE')
-												<!-- Button trigger modal -->
-												<a href="#" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  class="ms-3"><i class='bx bxs-trash'></i></a>
-												<!-- Modal -->
-												<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-												<div class="modal-dialog">
-													<div class="modal-content">
-													<div class="modal-header">
-														<h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure you want to delete this data</h1>
-														<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-													</div>
-													<div class="modal-fullscreen-xxl-down">
-													<label for="inputProductTitle" style="text-align:center" class="form-label">
-														Comment : 
-														{{ \Str::limit($comment->the_comment, 60) }} 
-													</label>
-													
-													<br/>
-													
-													</div>
-													<div class="modal-footer">
-														<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-														<button type="button" onclick="event.preventDefault(); document.getElementById('delete_form_{{ $comment->id }}').submit();" class="btn btn-primary">Delete</button>
-													</div>
-													</div>
-												</div>
-												</div>
-												</form>  --}}
-
-                                            </div>
-										</td>
+                                      
 									</tr>
                                     @endforeach
 								</tbody>
@@ -192,23 +153,6 @@
         });
 	</script>
     @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
