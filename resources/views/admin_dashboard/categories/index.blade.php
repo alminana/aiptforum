05c3f00f10687824a11c40ca9b6b3de4d8247f41
alminana <45486184+alminana@users.noreply.github.com>
2023-05-14 17:43:14 +0300

    Update index.blade.php

--- resources/views/admin_dashboard/categories/index.blade.php
+++ resources/views/admin_dashboard/categories/index.blade.php
@@ -1,7 +1,5 @@
 @extends("admin_dashboard.layouts.app")
-@section("style")
-	<link href="{{ asset('admin_dashboard_assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
-	@endsection
+		
 		@section("wrapper")
 		<!--start page wrapper -->
 		<div class="page-wrapper">
@@ -12,7 +10,7 @@
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
-								<li class="breadcrumb-item"><a href="{{ route('admin.categories.index') }}"><i class="bx bx-home-alt"></i></a>
+								<li class="breadcrumb-item"><a href="{{ route('admin.index') }}"><i class="bx bx-home-alt"></i></a>
 								</li>
 								<li class="breadcrumb-item active" aria-current="page">All Categories</li>
 							</ol>
@@ -23,66 +21,62 @@
 			  
 				<div class="card">
 					<div class="card-body">
+						<div class="d-lg-flex align-items-center mb-4 gap-3">
+							<div class="position-relative">
+								<input type="text" class="form-control ps-5 radius-30" placeholder="Search Order"> <span class="position-absolute top-50 product-show translate-middle-y"><i class="bx bx-search"></i></span>
+							</div>
+						  <div class="ms-auto"><a href="{{ route('admin.categories.create') }}" class="btn btn-primary radius-30 mt-2 mt-lg-0"><i class="bx bxs-plus-square"></i>Add New Category</a></div>
+						</div>
 						<div class="table-responsive">
-							<table id="example2" class="table table-striped table-bordered">
-								<thead>
-								<tr>
-									<th>Category#</th>
-									<th>Category Name</th>
-									<th>Actions</th>
+							<table class="table mb-0">
+								<thead class="table-light">
+									<tr>
+										<th>Category#</th>
+										<th>Category Name</th>
+										<th>Creator</th>
+                                        <th>Related Posts</th>
+										<th>Created at</th>
+										<th>Actions</th>
 									</tr>
 								</thead>
 								<tbody>
-								@forelse($categories as $category)
+                                    @foreach($categories as $category)
 									<tr>
-									<td>
+										<td>
 											<div class="d-flex align-items-center">
 												<div>
 													<input class="form-check-input me-3" type="checkbox" value="" aria-label="...">
 												</div>
 												<div class="ms-2">
-													<h6 class="mb-0 font-14">{{ $category->id }}</h6>
+													<h6 class="mb-0 font-14">#P-{{ $category->id }}</h6>
 												</div>
 											</div>
 										</td>
 										<td>{{ $category->name }} </td>
-                                        
+                                        <td>{{ $category->user->name }}</td>
+                                        <td>
+                                            <a class='btn btn-primary btn-sm' href="{{ route('admin.categories.show', $category) }}">Related Posts</a>
+                                        </td>
+                                        <td>{{ $category->created_at->diffForHumans() }}</td>
                                         <td>
 											<div class="d-flex order-actions">
 												<a href="{{ route('admin.categories.edit', $category) }}" class=""><i class='bx bxs-edit'></i></a>
+												<a href="#" onclick="event.preventDefault(); document.getElementById('delete_form_{{ $category->id }}').submit();" class="ms-3"><i class='bx bxs-trash'></i></a>
 											
-												<form method='post' action="{{ route('admin.categories.destroy', $category) }}" id='delete_form_{{ $category->id }}'>
-												@csrf 
-												@method('DELETE')
-												<!-- Button trigger modal -->
-												<a href="#" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"  class="ms-3"><i class='bx bxs-trash'></i></a>
-												<!-- Modal -->
-												<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-												<div class="modal-dialog">
-													<div class="modal-content">
-													<div class="modal-header">
-														<h1 class="modal-title fs-5" id="exampleModalLabel">Are you sure you want to delete this data</h1>
-														<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-													</div>
-													
-													<div class="modal-footer">
-														<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-														<button type="button" onclick="event.preventDefault(); document.getElementById('delete_form_{{ $category->id }}').submit();" class="btn btn-primary">Delete</button>
-													</div>
-													</div>
-												</div>
-												</div>
-												</form> 											
+                                                <form method='post' action="{{ route('admin.categories.destroy', $category) }}" id='delete_form_{{ $category->id }}'>@csrf @method('DELETE')</form>
                                             </div>
 										</td>
 									</tr>
-                                    @empty
-										<p class='lead'>There are no Client to show.</p>
-									@endforelse
+                                    @endforeach
 								</tbody>
 							</table>
-						
-				
+						</div>
+
+                        <div class='mt-4'>
+                        {{ $categories->links() }}
+                        </div>
+                        
+					</div>
 				</div>
 
 
@@ -103,132 +97,5 @@
 
         });
 
-
-		
-		$(function(){
-    $(document).on('click','#delete',function(e){
-        e.preventDefault();
-        var link = $(this).attr("href");
-
-  
-                  Swal.fire({
-                    title: 'Are you sure?',
-                    text: "Delete This Data?",
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    cancelButtonColor: '#d33',
-                    confirmButtonText: 'Yes, delete it!'
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                      window.location.href = link
-                      Swal.fire(
-                        'Deleted!',
-                        'Your file has been deleted.',
-                        'success'
-                      )
-                    }
-                  }) 
-
-
-    });
-
-  });
-
     </script>
-	  <script src="{{ asset('admin_dashboard_assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
-	<script src="{{ asset('admin_dashboard_assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
-	<script>
-		$(document).ready(function() {
-			var table = $('#example2').DataTable( {
-				lengthChange: false,
-				buttons: ['excel','copy']
-			} );
-		 
-			table.buttons().container()
-				.appendTo( '#example2_wrapper .col-md-6:eq(0)' );
-		
-            setTimeout(() => {
-                $(".general-message").fadeOut();
-            }, 5000);
-        
-        });
-
-		$(document).ready(function () {
-        
-		setTimeout(() => {
-			$(".general-message").fadeOut();
-		}, 5000);
-
-	});
-	</script>
     @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
