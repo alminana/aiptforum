f16c24f579b4e4e009f3de9101567122acf75bc9
alminana <45486184+alminana@users.noreply.github.com>
2023-05-15 16:11:36 +0300

    Update show.blade.php

--- resources/views/admin_dashboard/categories/show.blade.php
+++ resources/views/admin_dashboard/categories/show.blade.php
@@ -1,5 +1,7 @@
 @extends("admin_dashboard.layouts.app")
-		
+@section("style")
+	<link href="{{ asset('admin_dashboard_assets/plugins/datatable/css/dataTables.bootstrap5.min.css') }}" rel="stylesheet" />
+	@endsection
 		@section("wrapper")
 		<!--start page wrapper -->
 		<div class="page-wrapper">
@@ -12,7 +14,8 @@
 							<ol class="breadcrumb mb-0 p-0">
 								<li class="breadcrumb-item"><a href="{{ route('admin.index') }}"><i class="bx bx-home-alt"></i></a>
 								</li>
-								<li class="breadcrumb-item active" aria-current="page">Category Posts</li>
+								<li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.posts.create') }}">Add Category</a></li>
+
 							</ol>
 						</nav>
 					</div>
@@ -21,28 +24,30 @@
 			  
 				<div class="card">
 					<div class="card-body">
-						<div class="d-lg-flex align-items-center mb-4 gap-3">
-							<div class="position-relative">
-								<input type="text" class="form-control ps-5 radius-30" placeholder="Search Order"> <span class="position-absolute top-50 product-show translate-middle-y"><i class="bx bx-search"></i></span>
-							</div>
-						  <div class="ms-auto"><a href="{{ route('admin.posts.create') }}" class="btn btn-primary radius-30 mt-2 mt-lg-0"><i class="bx bxs-plus-square"></i>Add New Post</a></div>
-						</div>
 						<div class="table-responsive">
-							<table class="table mb-0">
-								<thead class="table-light">
-									<tr>
+							<table id="example2" class="table table-striped table-bordered">
+								<thead>
+								<tr>
 										<th>Post#</th>
-										<th>Post Title</th>
-										<th>Post Excerpt</th>
-										<th>Category</th>
-										<th>Created at</th>
-                                        <th>Status</th>
+										<th>Reference</th>
+										<th>Application</th>
+								
+										<th>Procedure</th>
+										<th>Filing No.</th>
+										<th>Client</th>
+										<th>Country</th>
+										<th>Class</th>
+								
+										<th>Actual Deadline</th>
+                                 
+                                    
 										<th>Views</th>
-						
+										<th>Created at</th>
+								
 									</tr>
 								</thead>
 								<tbody>
-                                    @foreach($category->posts as $post)
+									@foreach($category->posts as $post)
 									<tr>
 										<td>
 											<div class="d-flex align-items-center">
@@ -50,30 +55,27 @@
 													<input class="form-check-input me-3" type="checkbox" value="" aria-label="...">
 												</div>
 												<div class="ms-2">
-													<h6 class="mb-0 font-14">#P-{{ $post->id }}</h6>
+													<h6 class="mb-0 font-14">{{ $post->id }}</h6>
 												</div>
 											</div>
 										</td>
-										<td>{{ $post->title }} </td>
-
-										<td>{{ $post->excerpt }}</td>
-                                        <td>{{ $post->category->name }}</td>
+										<td>{{$post->aiptref}}</td>
+										<td>{{$post->title}}</td>
+										<td>{{$post->status}}</td>
+										<td>{{$post->slug}}</td>
+										<td>{{$post->excerpt}}</td>
+										<td>{{$post->country}}</td>
+										<td>{{$post->class}}</td>
+										<td>{{$post->proceduredate}}</td>
+										<td>{{ $post->views }}</td>
                                         <td>{{ $post->created_at->diffForHumans() }}</td>
-                                        
-                                        
-                                        <td>
-                                            <div class="badge rounded-pill @if($post->status === 'published') {{ 'text-info bg-light-info' }} @elseif($post->status === 'draft') {{ 'text-warning bg-light-warning' }} @else {{ 'text-danger bg-light-danger' }} @endif p-2 text-uppercase px-3"><i class='bx bxs-circle align-middle me-1'></i>{{ $post->status }}</div>
-                                        </td>
-
-                                        <td>{{ $post->views }}</td>
-                                        
-                                        
+                                      
 									</tr>
                                     @endforeach
 								</tbody>
 							</table>
-						</div>
-					</div>
+						
+				
 				</div>
 
 
@@ -94,5 +96,72 @@
 
         });
 
+
+		
+		$(function(){
+    $(document).on('click','#delete',function(e){
+        e.preventDefault();
+        var link = $(this).attr("href");
+
+  
+                  Swal.fire({
+                    title: 'Are you sure?',
+                    text: "Delete This Data?",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      window.location.href = link
+                      Swal.fire(
+                        'Deleted!',
+                        'Your file has been deleted.',
+                        'success'
+                      )
+                    }
+                  }) 
+
+
+    });
+
+  });
+
     </script>
+	  <script src="{{ asset('admin_dashboard_assets/plugins/datatable/js/jquery.dataTables.min.js') }}"></script>
+	<script src="{{ asset('admin_dashboard_assets/plugins/datatable/js/dataTables.bootstrap5.min.js') }}"></script>
+	<script>
+		$(document).ready(function() {
+			var table = $('#example2').DataTable( {
+				lengthChange: false,
+				buttons: ['excel','copy']
+			} );
+		 
+			table.buttons().container()
+				.appendTo( '#example2_wrapper .col-md-6:eq(0)' );
+		
+            setTimeout(() => {
+                $(".general-message").fadeOut();
+            }, 5000);
+        
+        });
+	</script>
     @endsection
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
