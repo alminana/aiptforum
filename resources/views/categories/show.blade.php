504f70f31df6355ce20c44c2f271dd4ab3b9f7b3
EILERalminana <alminanaeilerbutad20@gmail.com>
2023-07-20 11:44:20 +0300

    update application grid

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -45,75 +45,116 @@
 							<table id="tbAdresse" cellspacing="0" style="border:1px;" class="table table-striped table-bordered" role="grid" aria-describedby="tbAdresse_info">
 								<thead>
 								<tr role="row">
-							
-								<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">AIPTREF</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">AIPTREF</th>
 									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client Ref.</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Type</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Image</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Description</th>
 									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Application</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Class</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Registration</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Procedure</th>								
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Method</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Image</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Requested Date</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Actual Date</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Filing #</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Registration #</th>
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">class</th>						
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client</th>	
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Country</th>	
+									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Condition</th>						
+		
 							</tr>
 							</thead>
 								<tfoot>
 									<tr role="row">
-								
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">AIPTREF</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client Ref.</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Type</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Image</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Description</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Application</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Class</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Registration</th>
-									<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Procedure</th>
-									</tr>
-								</tfoot>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">AIPTREF</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client Ref.</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Application</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Method</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Image</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Requested Date</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Actual Date</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Filing #</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Registration #</th>
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">class</th>						
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Client</th>	
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Country</th>	
+										<th class="sorting" tabindex="0" aria-controls="tbAdresse" rowspan="1" colspan="1" style="width: 54px;">Condition</th>						
+										</tr>
+								</thead>
 							
 							<tbody>
 								@forelse($posts as $post)
-								<?php
-										$favcolor = ($post->body);
-										$color = "";
-										switch ($favcolor) {
-										case "Done":
-											$color = "color:black;background-color:#94F740;";
-																					
-											break;
-										case "New":
-											$color = "color:black;background-color:orange";	
-																				
-											break;
-										case "Process":
-											$color = "color:black;background-color:#F5FF53;";
-																			
-											break;
-										case "Rejected":
-											$color = "color:black;background-color:#9440F7;";
-											break;								
-										default:
-											$color = "color:black;background-color:White;";
-																			
-										}
-											?>
-							<tr role="row" class="odd">
+						
+							@php
+								$remaining = \Carbon\Carbon::now()->diffInDays($post->proceduredate);
+								$expire = strtotime($post->proceduredate);
+								$today = strtotime("today midnight");
+								$day_diff = $today - $expire;                                    
+								$color = "";
 							
-								<td style="font-size:12px;"><a style="color:black;" href="{{ route('posts.show', $post) }}">{{$post->aiptref}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->clientref}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->excerpt}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->category->name}}</a></td>
-								<td style="font-size:12px;">
+								if ($today >= $expire) {
+									$color = "color:black;background-color:#D99594;";
+								}elseif ($remaining < 7 ) {
+									$color = "color:black;background-color:#FFFF00;";
+								} elseif ($expire >= 30) {
+									$color = "color:black;background-color:#92D050;";
+								}
+							@endphp
+							<tr role="row" class="odd">
+								<td class="aiptref" href="{{ route('posts.show', $post) }}" style="{{ $color }}">{{  $post->aiptref }}</td>
+								<td class="clientref" href="{{ route('posts.show', $post) }}" style="{{ $color }}">{{ $post->clientref }}</td>
+								<td class="title" href="{{ route('posts.show', $post) }}" style="{{ $color }}">{{ $post->title }}</td>
+
+							
+								<td class="" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->status }}</td>
+								<td style="font-size:11px;{{ $color }};">
 									<img style='width: 100%' src="/storage/{{ $post->image ? $post->image->path : 'placeholders/thumbnail_placeholder.svg' }}" class='img-responsive' alt="Post Thumbnail">
+
+									{{-- <a style="color:black;"href="{{ route('posts.show', $post) }}">
+
+										<img style='width: 60%' src="{{Storage::disk('s3')->temporaryUrl($post->image->path, now()->addMinutes(20))}}" />
+							
+									</a> --}}
 								</td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->title}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->class}}</a></td>
-								<td style="font-size:1px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->slug}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->registrationno}}</a></td>
-								<td style="font-size:12px;"><a style="color:black;"href="{{ route('posts.show', $post) }}">{{$post->status}}</a></td>
+								<td class="requesteddate" style="{{ $color }}">
+									<a href="{{ route('admin.posts.edit', $post) }}"><button  class="btn btn-success">{{ $post->requesteddate }}</button></a>
+									
+								</td>
+
+								<td class="proceduredate" style="{{ $color }}">
+									<a href="{{ route('admin.posts.edit', $post) }}"><button  class="btn btn-primary">{{ $post->proceduredate }}</button></a>
+
+								</td>
+								
+								<td class="slug" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->slug }}</td>
+								<td class="registrationno" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->registrationno }}</td>
+								<td class="class" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->class }}</td>
+								<td class="excerpt" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->excerpt }}</td>
+								<td class="country" style="{{ $color }}" href="{{ route('posts.show', $post) }}">{{ $post->country }}</td>
+						   
+
+								<td class="deadline" href="{{ route('posts.show', $post) }}" style="{{ $color }}">
+									<p>	
+										@php
+											$remaining = \Carbon\Carbon::now()->diffInDays($post->proceduredate);
+											$expire = strtotime($post->proceduredate);
+											$today = strtotime("today midnight");
+											$day_diff = $today - $expire;                                    
+											$color = "";
+										
+											if ($today >= $expire) {
+												echo "deadline";
+												$color = "color:black;background-color:#D99594;";
+											}elseif ($remaining < 7 ) {
+												echo "upcoming";
+												$color = "color:black;background-color:#FFFF00;";
+											} elseif ($expire >= 30) {
+												echo "safe";
+												$color = "color:black;background-color:#92D050;";
+											}
+										@endphp
+									
+									</p>
+										 
+								</td>
+
 						
 									</a>
 								</td>  
