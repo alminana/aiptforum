a6d269c1b6714215db4bec4b8b49b75cfabcc222
alminana <45486184+alminana@users.noreply.github.com>
2023-10-23 23:40:49 +0300

    Update annual.blade.php

--- resources/views/past/annual.blade.php
+++ resources/views/past/annual.blade.php
@@ -70,7 +70,7 @@
                                                       $done = "done";
                                                       if(($expire == $default)){
                                                      
-                                                      $color = "color:black;background-color:green;";
+                                                      $color = "display:none";
                                                     }elseif($today == $expire){
                                                  
                                                       $color = "color:black;background-color:orange;";
@@ -103,7 +103,7 @@
                                                       $done = "done";
                                                       if(($expire == $default)){
                                                       echo "No Deadline";
-                                                      $color = "color:black;background-color:green;";
+                                                      $color = "display:none";
                                                     }elseif($today == $expire){
                                                       echo "DueDate ";
                                                       $color = "color:black;background-color:orange;";
