edfc5aef5e046c9492e05762fae7620c9e343362
alminana <alminanaeilerbutad20@gmail.com>
2023-10-28 16:01:25 +0300

    add new

--- resources/views/deadline/dashboard.blade.php
+++ resources/views/deadline/dashboard.blade.php
@@ -222,12 +222,7 @@
                                                                                       
                                                                                     </div>
                                                                                 </div>
-                                                                            </div>
-                                                                                </div>
-                                                                                <div class="modal-footer">
-                                                                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                                                                    <button type="button" class="btn btn-primary">Save changes</button>
-                                                                                </div>
+                                                                           
                                                                             </div>
                                                                         </div>
                                                                     </div>
