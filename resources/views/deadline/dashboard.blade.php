5a1329f1859ac8e8f5e737c7de5a5df1dbcacecd
alminana <alminanaeilerbutad20@gmail.com>
2023-10-28 15:59:26 +0300

    add new

--- resources/views/deadline/dashboard.blade.php
+++ resources/views/deadline/dashboard.blade.php
@@ -157,76 +157,69 @@
                                                                     </div>
                                                       
                                                                 <!--end modal-->
-                                                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                                        <div class="modal-dialog">
+                                                                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"  aria-hidden="true">
+                                                                        <div class="modal-dialog  modal-lg">
                                                                             <div class="modal-content">
-                                                                                <div class="modal-header">
+                                                                                <div class="modal-header border-top border-0 border-4 border-info">
                                                                                     <h5 class="modal-title" id="exampleModalLabel">Update Search Report</h5>
-                                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"  aria-label="Close"></button>
                                                                                 </div>
                                                                                 <div class="modal-body">
-                                                                                    <div class=" border-top border-0 border-4 border-info">
-                                                                                        <div class="card-body">
+                                                                           
+                                                                                       
                                                                                             <div class="border p-4 rounded">
-                                                                                                <div class="card-title d-flex align-items-center">
-                                                                                                    <div><i class="bx bxs-user me-1 font-22 text-info"></i>
-                                                                                                    </div>
-                                                                                                    <h5 class="mb-0 text-info">User Registration</h5>
-                                                                                                </div>
-                                                                                                <hr/>
+                                                                                              
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputEnterYourName" class="col-sm-3 col-form-label">Enter Your Name</label>
+                                                                                                    <label for="inputEnterYourName" class="col-sm-3 col-form-label">Aipt Reference</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <input type="text" class="form-control" id="inputEnterYourName" placeholder="Enter Your Name">
+                                                                                                        <input type="text" class="form-control" id="inputEnterYourName" placeholder="Aipt Reference">
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputPhoneNo2" class="col-sm-3 col-form-label">Phone No</label>
+                                                                                                    <label for="inputPhoneNo2" class="col-sm-3 col-form-label">Client Reference</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <input type="text" class="form-control" id="inputPhoneNo2" placeholder="Phone No">
+                                                                                                        <input type="text" class="form-control" id="inputPhoneNo2" placeholder="Client Reference">
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputEmailAddress2" class="col-sm-3 col-form-label">Email Address</label>
+                                                                                                    <label for="inputEmailAddress2" class="col-sm-3 col-form-label">Country</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <input type="email" class="form-control" id="inputEmailAddress2" placeholder="Email Address">
+                                                                                                        <input type="email" class="form-control" id="inputEmailAddress2" placeholder="Country">
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputChoosePassword2" class="col-sm-3 col-form-label">Choose Password</label>
+                                                                                                    <label for="inputEmailAddress2" class="col-sm-3 col-form-label">Procedure</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <input type="email" class="form-control" id="inputChoosePassword2" placeholder="Choose Password">
+                                                                                                        <input type="email" class="form-control" id="inputEmailAddress2" placeholder="Procedure">
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputConfirmPassword2" class="col-sm-3 col-form-label">Confirm Password</label>
+                                                                                                    <label for="inputChoosePassword2" class="col-sm-3 col-form-label">Examination Report</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <input type="email" class="form-control" id="inputConfirmPassword2" placeholder="Confirm Password">
+                                                                                                        <input type="email" class="form-control" id="inputChoosePassword2" placeholder="Examination Report>
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputAddress4" class="col-sm-3 col-form-label">Address</label>
+                                                                                                    <label for="inputConfirmPassword2" class="col-sm-3 col-form-label">Registrability</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <textarea class="form-control" id="inputAddress4" rows="3" placeholder="Address"></textarea>
+                                                                                                        <input type="email" class="form-control" id="inputConfirmPassword2" placeholder="Registrability">
                                                                                                     </div>
                                                                                                 </div>
                                                                                                 <div class="row mb-3">
-                                                                                                    <label for="inputAddress4" class="col-sm-3 col-form-label"></label>
+                                                                                                    <label for="inputAddress4" class="col-sm-3 col-form-label">Opinion</label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <div class="form-check">
-                                                                                                            <input class="form-check-input" type="checkbox" id="gridCheck4">
-                                                                                                            <label class="form-check-label" for="gridCheck4">Check me out</label>
-                                                                                                        </div>
+                                                                                                        <textarea class="form-control" id="inputAddress4" rows="3" placeholder="Opinion"></textarea>
                                                                                                     </div>
                                                                                                 </div>
+                                                                                               
                                                                                                 <div class="row">
                                                                                                     <label class="col-sm-3 col-form-label"></label>
                                                                                                     <div class="col-sm-9">
-                                                                                                        <button type="submit" class="btn btn-info px-5">Register</button>
+                                                                                                        <button type="submit" class="btn btn-info px-5">Update</button>
                                                                                                     </div>
                                                                                                 </div>
                                                                                             </div>
-                                                                                        </div>
+                                                                                      
                                                                                     </div>
                                                                                 </div>
                                                                             </div>
